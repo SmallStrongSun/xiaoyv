3b4c8c425dc81073df0e41fd6c11733f86840cd1
xiaoyv <sxqlzxy@163.com>
2020-07-27 22:51:50 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,20 @@
+"""
+print("hello world!")  #字符串
+print(123)#整数
+print(222.222)#浮点型
+print(False)#布尔型
+print(True)#布尔型
+print(())#元组
+print([])#数组
+print({})#字典
+print("不识庐山真面目",121212,True)
+
+会当临绝顶
+一览众山小
+"""
+print("哈哈"*100) 
+print(type("hahha"))
+
+a=len(input("请输入："))
+b=len(input("请输入：")) 
+print("结果：",a+b)
